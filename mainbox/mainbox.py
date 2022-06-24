6a8dcd490d3661b12b07c415ab93f9594c918933
costa-rica <nrodrig1@gmail.com>
2022-06-24 13:23:30 +0200

    functionality update: mainbox layout has a self.sc (size coefficient) which selects 1,2 or 3 from the size_dict dictionary. This determines the size of font, padding, etc. 1 small, 3 large

--- mainbox/mainbox.py
+++ mainbox/mainbox.py
@@ -23,11 +23,13 @@ class MainBoxLayout(BoxLayout):
   def __init__(self,**kwargs):
     super(MainBoxLayout, self).__init__(**kwargs)
     self.on_size_count=0
+    self.sc=3#size coefficient
     self.bind(size=self.on_size)
+
 
   def on_size(self,*args):
     print('MainBox on_size')
-
+    self.input_act_note.sc=self.sc
     if self.on_size_count==1:
       print('MainBox on_size - made changes ****')
       self.label_email.text="nickapeed@yahoo.com"
@@ -48,14 +50,14 @@ class MainBoxLayout(BoxLayout):
 
   # Act Screen Name / Email
     self.label_email.size_hint=(None,None)
-    self.label_email.font_size=self.ps1_base_height * size_dict['label_email']['font_size'][2]
+    self.label_email.font_size=self.ps1_base_height * size_dict['label_email']['font_size'][self.sc]
     self.label_email.size= self.label_email.texture_size
 
     self.anchor_screen_name.anchor_x="right"
     self.anchor_screen_name.size_hint=(1,None)
-    self.anchor_screen_name.padding=(0,self.ps1_base_height * size_dict['anchor_screen_name']['padding-top'][1],
-      self.ps1_base_width * size_dict['anchor_screen_name']['padding-right'][2],0)
-    self.anchor_screen_name.height=self.label_email.height + self.ps1_base_height * size_dict['anchor_screen_name']['padding-top'][1]
+    self.anchor_screen_name.padding=(0,self.ps1_base_height * size_dict['anchor_screen_name']['padding-top'][self.sc],
+      self.ps1_base_width * size_dict['anchor_screen_name']['padding-right'][self.sc],0)
+    self.anchor_screen_name.height=self.label_email.height + self.ps1_base_height * size_dict['anchor_screen_name']['padding-top'][self.sc]
 
     Clock.schedule_once(self.get_size, .01)
     # print('Note: Anything after this line will run concurrently, so just put everything in get_size from now on...')
@@ -65,7 +67,7 @@ class MainBoxLayout(BoxLayout):
 
   #Add Act Name
     self.label_act_name.size_hint=(None,None)
-    self.label_act_name.font_size = self.ps1_base_width * size_dict['label_act_name']['font_size'][2]
+    self.label_act_name.font_size = self.ps1_base_width * size_dict['label_act_name']['font_size'][self.sc]
     self.anchor_screen_name.size_hint_y=None
     Clock.schedule_once(self.get_size_2, .01)
     # print('Note: Anything after this line will run concurrently, so just put everything in get_size_2 from now on...')
@@ -80,7 +82,7 @@ class MainBoxLayout(BoxLayout):
     #   Color(1, 0, 0, 1)
     #   Rectangle(pos=self.label_email.pos, size=self.label_email.size)
 
-    self.input_act_name.font_size=self.ps1_base_width * size_dict['input_act_name']['font_size'][2]
+    self.input_act_name.font_size=self.ps1_base_width * size_dict['input_act_name']['font_size'][self.sc]
     self.input_act_name.height = self.label_act_name.height
 
     self.anchor_act_name_label.anchor_x="left"
@@ -89,20 +91,20 @@ class MainBoxLayout(BoxLayout):
 
     self.box_act_name.size_hint=(1,None)
     self.box_act_name.height = self.anchor_act_name_label.height + self.input_act_name.height + \
-      self.ps1_base_height * size_dict['box_act_name']['padding-top'][2]
-    self.box_act_name.padding =(self.ps1_base_width*size_dict['box_act_name']['padding-left'][2],
-      self.ps1_base_height * size_dict['box_act_name']['padding-top'][2],
-      self.ps1_base_width*size_dict['box_act_name']['padding-right'][2],0)
+      self.ps1_base_height * size_dict['box_act_name']['padding-top'][self.sc]
+    self.box_act_name.padding =(self.ps1_base_width*size_dict['box_act_name']['padding-left'][self.sc],
+      self.ps1_base_height * size_dict['box_act_name']['padding-top'][self.sc],
+      self.ps1_base_width*size_dict['box_act_name']['padding-right'][self.sc],0)
 
   #Add Act Note
     self.label_act_note.size_hint=(None,None)
-    self.label_act_note.font_size = self.ps1_base_width * size_dict['label_act_note']['font_size'][2]
+    self.label_act_note.font_size = self.ps1_base_width * size_dict['label_act_note']['font_size'][self.sc]
     Clock.schedule_once(self.get_size_3,.01)
 
   def get_size_3(self,*args):
     self.label_act_note.size=self.label_act_note.texture_size
 
-    self.input_act_note.font_size=self.ps1_base_width * size_dict['input_act_note']['font_size'][2]
+    self.input_act_note.font_size=self.ps1_base_width * size_dict['input_act_note']['font_size'][self.sc]
     self.input_act_note.size_hint=(1,None)
     self.input_act_note.height=self.label_act_note.height *len(self.input_act_note._lines)
 
@@ -111,13 +113,13 @@ class MainBoxLayout(BoxLayout):
     self.anchor_act_note_label.height = self.label_act_note.height
 
     self.box_act_note.size_hint=(1,None)
-    self.box_act_note.padding=(self.ps1_base_width*size_dict['box_act_note']['padding-left'][2],
-      self.ps1_base_height * size_dict['box_act_note']['padding-top'][2],
-      self.ps1_base_width*size_dict['box_act_note']['padding-right'][2],0)
+    self.box_act_note.padding=(self.ps1_base_width*size_dict['box_act_note']['padding-left'][self.sc],
+      self.ps1_base_height * size_dict['box_act_note']['padding-top'][self.sc],
+      self.ps1_base_width*size_dict['box_act_note']['padding-right'][self.sc],0)
 
   #Add Date Time
-    self.label_time.font_size = self.ps1_base_width * size_dict['label_time']['font_size'][2]
-    self.label_date.font_size = self.ps1_base_width * size_dict['label_date']['font_size'][2]
+    self.label_time.font_size = self.ps1_base_width * size_dict['label_time']['font_size'][self.sc]
+    self.label_date.font_size = self.ps1_base_width * size_dict['label_date']['font_size'][self.sc]
 
     self.label_time.size_hint = (None,None)
     self.label_date.size_hint = (None,None)
@@ -128,8 +130,8 @@ class MainBoxLayout(BoxLayout):
     self.label_date.size = self.label_date.texture_size
     self.label_time.size = self.label_time.texture_size
 
-    self.input_time.font_size = self.ps1_base_width * size_dict['input_time']['font_size'][2]
-    self.input_date.font_size = self.ps1_base_width * size_dict['input_date']['font_size'][2]
+    self.input_time.font_size = self.ps1_base_width * size_dict['input_time']['font_size'][self.sc]
+    self.input_date.font_size = self.ps1_base_width * size_dict['input_date']['font_size'][self.sc]
     self.input_time.size_hint=(1,None)
     self.input_date.size_hint=(1,None)
 
@@ -142,15 +144,15 @@ class MainBoxLayout(BoxLayout):
 
     self.anchor_date_time.size_hint=(1,None)
     self.anchor_date_time.height = self.input_date.height + self.label_date.height + \
-      self.ps1_base_height * size_dict['anchor_date_time']['padding-top'][2]
-    self.anchor_date_time.padding =(self.ps1_base_width * size_dict['anchor_date_time']['padding-left'][2],
-      self.ps1_base_height * size_dict['anchor_date_time']['padding-top'][2],
-      self.ps1_base_width * size_dict['anchor_date_time']['padding-right'][2],0)
+      self.ps1_base_height * size_dict['anchor_date_time']['padding-top'][self.sc]
+    self.anchor_date_time.padding =(self.ps1_base_width * size_dict['anchor_date_time']['padding-left'][self.sc],
+      self.ps1_base_height * size_dict['anchor_date_time']['padding-top'][self.sc],
+      self.ps1_base_width * size_dict['anchor_date_time']['padding-right'][self.sc],0)
 
-    self.btn_submit_act.font_size = self.ps1_base_width * size_dict['btn_submit_act']['font_size'][2]
+    self.btn_submit_act.font_size = self.ps1_base_width * size_dict['btn_submit_act']['font_size'][self.sc]
 
     self.anchor_submit.anchor_x = "right"
-    self.anchor_submit.padding = (0,0,self.ps1_base_width * size_dict['anchor_submit']['padding-right'][2],0)
+    self.anchor_submit.padding = (0,0,self.ps1_base_width * size_dict['anchor_submit']['padding-right'][self.sc],0)
 
 
   def wonder_button(self):
@@ -196,7 +198,7 @@ class TextInputDynamicActNote(TextInput):
     self.main_box=self.parent.parent
     self.height=len(self._lines) * self.main_box.label_act_note.height
     self.main_box.box_act_note.height=self.main_box.input_act_note.height + self.main_box.anchor_act_note_label.height + \
-      self.main_box.ps1_base_height * size_dict['box_act_note']['padding-top'][2]
+      self.main_box.ps1_base_height * size_dict['box_act_note']['padding-top'][self.sc]
 
 
   def on_focus(self, instance_twice, value):
